1d070ef85c6ce8a45995bf3ad03cf2d3e99a8202
indrajeet <indrajeet.kumar@rigzone.com>
2019-08-07 16:59:55 +0530

    Adding Install Class

---
+++ manifests/install.pp
@@ -0,0 +1,15 @@
+# Class: Nginx
+#
+#
+class nginx::install {
+  package {'nginx':
+  ensure   => present,
+
+  }
+}
+
+service {'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
